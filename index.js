5a9449e6f3a48596e4d15769ff93ba89006521a2
AbeHaruhiko <caliconography@gmail.com>
2017-12-26 08:22:51 +0000

    async/awaitに書き換え

--- index.js
+++ index.js
@@ -3,39 +3,28 @@
 const client = require('cheerio-httpcli');
 let inouttime = {};
 
-function easyInput(user, pass, mode, time) {
+async function easyInput(user, pass, mode, time) {
 
   // ログイン
-  return client.fetch('https://s2soft.co.jp/prbbs/login', { userId: user, userPass: pass })
-    .then((result) => {
-      // token取得のため、一度アクセスする
-      return client.fetch('https://s2soft.co.jp/prbbs/easyinput');
-    })
-    .then((result) => {
-      // 簡易入力
-      const inputInfo = {
-        'mode': mode,
-        'intime': time ? time : '自動'
-      };
-      return result.$('form[name=timeCard]').attr('method', 'POST').submit(inputInfo);
-    })
-    .then((result) => {
-      // 入力完了後、同じ画面に遷移してくる。同画面の開始時間、終了時間を読み取って inouttime オブジェクトへ保管。
-      const intime = result.$('dt:contains("開始時間")').next('dd').text();
-      const outtime = result.$('dt:contains("終了時間")').next('dd').text();
-      inouttime.intime = intime;
-      inouttime.outtime = outtime;
-      // console.log(inouttime);
-      return inouttime;
-    })
-    .catch(function (err) {
-      console.log(err);
-      return 'エラー'
-    })
-    .finally(function () {
-      // 処理完了でもエラーでも最終的に必ず実行される
-      client.reset();
-    });
+  await client.fetch('https://s2soft.co.jp/prbbs/login', { userId: user, userPass: pass });
+
+  // token取得のため、一度アクセスする
+  const easyinput = await client.fetch('https://s2soft.co.jp/prbbs/easyinput');
+
+  // 簡易入力
+  const inputInfo = {
+    'mode': mode,
+    'intime': time ? time : '自動'
+  };
+  const inputresult = await easyinput.$('form[name=timeCard]').attr('method', 'POST').submit(inputInfo);
+
+  // 入力完了後、同じ画面に遷移してくる。同画面の開始時間、終了時間を読み取って inouttime オブジェクトへ保管。
+  const intime = inputresult.$('dt:contains("開始時間")').next('dd').text();
+  const outtime = inputresult.$('dt:contains("終了時間")').next('dd').text();
+  inouttime.intime = intime;
+  inouttime.outtime = outtime;
+  // console.log(inouttime);
+  return inouttime;
 }
 
 module.exports = {
--- test.js
+++ test.js
@@ -4,6 +4,6 @@ const easyInput = require('./index.js');
 // Promiseが返ってくる
 const promise = easyInput.easyInput('h-abe', '', 'start', '18:30');
 promise.then((result) => {
-  console.log(result);
+  console.log(`出勤時刻: ${result.intime}, 退勤時刻: ${result.outtime}`);
   console.log('テストが正常に完了しました');
 });
